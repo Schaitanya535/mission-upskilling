f26251765540288a5b0fe54d19333eea37250eec
Chaitanya <seschaitanya535@gmail.com>
2026-06-08 12:16:42 +0530

    Median Stream problme solved.

--- dsa-python/heaps/median_stream.py
+++ dsa-python/heaps/median_stream.py
@@ -1,3 +1,6 @@
+import math
+import heapq
+
 """
 Problem 4 — Find median from a data stream.
 
@@ -28,19 +31,81 @@ Target: O(log n) add, O(1) median.
 
 
 class MedianFinder:
+    __max_heap: list[int]
+    __min_heap: list[int]
+    # __size: int
+
     def __init__(self) -> None:
-        raise NotImplementedError
+        self.__max_heap = []
+        self.__min_heap = []
+        # self.__size = 0
+        # raise NotImplementedError
 
     def add(self, num: int) -> None:
-        raise NotImplementedError
+        max_heap_len = len(self.__max_heap)
+        min_heap_len = len(self.__min_heap)
+
+        if max_heap_len == 0:
+            self.__max_heap.append(num)
+            # self.__size += 1
+            return
+
+        if min_heap_len == 0:
+            if num >= self.__max_heap[0]:
+                self.__min_heap.append(num)
+            else:
+                prev_left_middle = heapq.heapreplace_max(self.__max_heap, num)
+                self.__min_heap.append(prev_left_middle)
+            # self.__size += 1
+            return
+
+        left_middle = self.__max_heap[0]
+
+        if num < left_middle:
+            if max_heap_len > min_heap_len:
+                prev_left_middle = heapq.heappushpop_max(self.__max_heap, num)
+                heapq.heappush(self.__min_heap, prev_left_middle)
+            else:
+                heapq.heappush_max(self.__max_heap, num)
+        else:
+            if min_heap_len > max_heap_len:
+                prev_right_middle = heapq.heappushpop(self.__min_heap, num)
+                heapq.heappush_max(self.__max_heap, prev_right_middle)
+            else:
+                heapq.heappush(self.__min_heap, num)
+        # self.__size += 1
+        return
+        # raise NotImplementedError
 
     def median(self) -> float:
-        raise NotImplementedError
+        left_side = self.__max_heap
+        right_side = self.__min_heap
+
+        median = 0.0
+        if len(left_side) < len(right_side):
+            median = right_side[0]
+        elif len(right_side) < len(left_side):
+            median = left_side[0]
+        else:
+            median = (left_side[0] + right_side[0]) / 2
+        # print(median)
+        return median
+
+        # raise NotImplementedError
 
 
 def _brute_median(seen: list[int]) -> float:
-    """Oracle helper. Sort + pick middle(s)."""
-    raise NotImplementedError
+    # """Oracle helper. Sort + pick middle(s)."""
+    # raise NotImplementedError
+    seen.sort()
+    size = len(seen)
+    mid = math.floor(size / 2)
+    median: float = 0.0
+    if size % 2 == 1:
+        median = seen[mid]
+    else:
+        median = (seen[mid] + seen[mid - 1]) / 2
+    return median
 
 
 if __name__ == "__main__":
